2efa92f9bcd20fa408c5be741644e2d90f8227e8
samuelsimun <samuelsimun@gmail.com>
2022-10-27 13:25:41 +0200

    initial commit

--- Teamos/Teamos/settings.py
+++ Teamos/Teamos/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sv3w60v+pj4b1jxte(7%9(q#5bxwa9e8df=-c-j8^7n51qlkt('
+SECRET_KEY = 'django-insecure-6ueot4iza3qdflwc-qu2b7ale#hh2qy7!ag0*cy(kneb6le14i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
