40f1c69ab7533cc0597dcde93e3ec88f7ffc7898
samuelsimun <samuelsimun@gmail.com>
2022-11-27 12:54:28 +0100

    Added everythink

--- Teamos/home_site/urls.py
+++ Teamos/home_site/urls.py
@@ -4,5 +4,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # path("register/", views.register, name="register"),
+    path("logout", views.logout_request, name="logout"),
+    path("login", views.login_request, name="login"),
+    # path("user_home", views.user_home, name="user_home")
 ]
--- Teamos/home_site/views.py
+++ Teamos/home_site/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from . import views
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, 'home_site/Home.html')
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("home_site:index")
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('"home_site/login.html"')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request,
+                  template_name = "home_site/login.html",
+                  context={"form": form})
+
+# def user_home(request):
+#     return render(request=request,
+#                   template_name = "home_site/user_home.html",
+#                   contex=)
+
+
