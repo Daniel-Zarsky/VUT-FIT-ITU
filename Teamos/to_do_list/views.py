f056f3af5612da398d4e4eea4e5331d37693b130
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2022-12-12 10:38:23 +0100

    database change

--- Teamos/to_do_list/views.py
+++ Teamos/to_do_list/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def index(request):
+async def index(request):
     return HttpResponse("TO DO List")
