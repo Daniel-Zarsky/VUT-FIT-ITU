db48197de0dcabb7728fb607efaef2f3cc6bec88
Daniel-Zarsky <xzarsk04@stud.fit.vutbr.cz>
2022-12-12 15:09:03 +0100

    binding between pages done

--- Teamos/to_do_list/urls.py
+++ Teamos/to_do_list/urls.py
@@ -1,9 +1,13 @@
+
 from django.contrib import admin
 from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path('', views.tasklist, name='taskList'),
+    path("addTask", views.addTask, name='addTask'),
+    path("submit_task", views.submit_task, name='submit_task'),
+    path("delete_task", views.delete_task, name='delete_task'),
+    path("save_task", views.save_task, name='save_task')
 ]
--- Teamos/to_do_list/views.py
+++ Teamos/to_do_list/views.py
@@ -1,6 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
+def tasklist(request):
+    return render(request, 'to_do_list/Page-1.html')
 
-# Create your views here.
-async def index(request):
-    return HttpResponse("TO DO List")
+def addTask(request):
+    return render(request, 'to_do_list/Page-2.html')
+
+def delete_task(request):
+    return render(request, 'to_do_list/Page-1.html')
+
+def save_task(request):
+    # save task to database but do not send others
+    return render(request, 'to_do_list/Page-1.html')
+
+def submit_task(request):
+    # save task to database and send others
+    return render(request, 'to_do_list/Page-1.html')
