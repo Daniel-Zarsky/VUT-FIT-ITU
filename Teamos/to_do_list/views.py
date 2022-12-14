5dcd09abe0a251d9227e81ccf5be3080982c02c0
Daniel Žárský <100444468+Daniel-Zarsky@users.noreply.github.com>
2022-12-14 11:51:22 +0100

    fixing

--- Teamos/to_do_list/views.py
+++ Teamos/to_do_list/views.py
@@ -2,35 +2,36 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Task
 #from django.views.generic.list import ListView
+#from .forms import NameForm
+from django.http import HttpResponseRedirect
+
 @login_required(login_url='login')
 def tasklist(request):
-    return render(request, 'to_do_list/Page-1.html')
+    data = Task.objects.filter(user=request.user.username, done=False)
+    print(data)
+    return render(request, 'to_do_list/Page-1.html', {'data': data})
 
 # class List_of_tasks(ListView):
-#     model = Task
-#     context_name = 'tasks'
+#    model = Task
+#    context_object_name = 'tasks'
 
 def addTask(request):
     if request.method == 'POST':
         name = request.POST['task_name']
-        description= request.POST['task_description']
+        description = ['team_name']
         user = request.POST['user_name']
         team = request.POST['team_name']
         deadline = request.POST['date']
         priority = request.POST['priority']
 
         task = Task(name=name, description=description, user=user, team=team, deadline=deadline, priority=priority)
-
-     # if (Task.priority > "10" or Task.priority < "0"):
-     #      return HttpResponse('Error priority outside of the bounds.(0-10)')
-
         task.save()
+        return tasklist(request)
 
-        return render(request, 'to_do_list/Page-1.html')
     return render(request, 'to_do_list/Page-2.html')
 
-def delete_task(request):
-    return render(request, 'to_do_list/Page-1.html')
+# def delete_task(request):
+#     return render(request, 'to_do_list/Page-1.html')
 
 def save_task(request):
     # save task to database but do not send others
@@ -41,6 +42,44 @@ def submit_task(request):
     return render(request, 'to_do_list/Page-1.html')
 
 
+# def get_name(request):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = NameForm(request.POST)
+#         # check whether it's valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/thanks/')
+#
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         form = NameForm()
+#
+#     return render(request, 'name.html', {'form': form})
 
+
+def home(request):
+    return render(request, 'user_home/home.html')
+
+
+def view_teams(request):
+    return render(request, 'teams/list.html')
+
+
+def view_projects(request): #todo
+    return render(request, 'home_site/About.html')
+
+def process_completed(request):
+         name = request.GET.get('task_name')
+         data = Task.objects.filter(name=name)
+         print("input")
+         print(data)
+         data.update(done=True, priority=0)
+         print("updated")
+         print(data)
+         return tasklist(request)
 
 
