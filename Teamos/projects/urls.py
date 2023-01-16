c54f7c18ae49f40517570196887c6fc15595ac55
samuelsimun <samuelsimun@gmail.com>
2023-01-16 11:40:53 +0100

    Added basic functions

--- Teamos/projects/urls.py
+++ Teamos/projects/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('show_timeline', views.show_timeline, name='show_timeline'),
     path('manage_deadlines', views.manage_deadlines, name='manage_deadlines'),
     path('delete_project', views.delete_project, name='delete_project'),
-    path('delete_deadline', views.delete_deadline, name='delete_deadline')
+    path('delete_deadline', views.delete_deadline, name='delete_deadline'),
+    path('validate_team', views.validate_team, name='validate_team')
     ]
